270c628c7df2210fe646b0bf9b35ff0ec8875978
therahulsonawane <rahulsonwane7066@gmail.com>
2024-09-03 07:14:45 +0530

    first project complete

---
+++ FirstProject/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import HeroSection from "./component/Hero";
+import Navigation from "./component/navigation";
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
